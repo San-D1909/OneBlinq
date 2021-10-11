2719716fd249d8f451d69ccbebcc58c3f352a1ea
Ryan Bours <ryan.bours@gmail.com>
2021-10-11 13:50:55 +0200

    Move ping to seperate controller reanbled swagger removed .vs from git cache added local config

---
+++ Backend/Controllers/GumroadController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Backend.Models;
+using System;
+using Backend.Infrastructure.Data;
+using System.Linq;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class GumroadController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GumroadController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        //[ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Ping(GumroadResponse response)
+        {
+            User user = _context.User.Where(u => u.Email == response.Email).FirstOrDefault();
+            if (user == null)
+            {
+                user = new User()
+                {
+                    Email = response.Email,
+                    FullName = response.Full_Name,
+                    Password = "",
+                    UserName = ""
+                };
+                _context.User.Add(user);
+                _context.SaveChanges();
+            }
+
+            License license = new License()
+            {
+                ExpirationDate = new DateTime(2021, 11, 05),
+                LicenseType = "Test",
+                IfActive = true,
+                TimesActivated = 1,
+                LicenseId = response.License_Key
+            };
+
+            license.UserId = user.UserId;
+
+            _context.License.Add(license);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -35,20 +35,20 @@ namespace Backend
 
 
             services.AddControllers();
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Backend", Version = "v1" });
-            //});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Backend", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("local"))
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend v1"));
             }
 
             app.UseHttpsRedirection();
