b8b568245638c5e104982226f94835df5f5d8ed8
Jorrit Deschaux <458871@student.fontys.nl>
2021-10-18 14:31:20 +0200

    login action created with JWT token

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -1,6 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Infrastructure.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Ubiety.Dns.Core;
 
 namespace Backend.Controllers
 {
@@ -9,16 +21,52 @@ namespace Backend.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        [HttpGet("LogIn")]
-        public IActionResult LogIn()
+		private readonly ApplicationDbContext _context;
+
+		public AuthController(ApplicationDbContext context)
+		{
+            _context = context;
+		}
+
+
+		[HttpPost("LogIn")]
+        public async Task<IActionResult> LogIn([FromBody] LoginModel credentials)
         {
-            throw new NotImplementedException();
+            var user =  await _context.User
+                    .Where(u => u.Email == credentials.Mail && u.Password == credentials.Password)
+                    .FirstOrDefaultAsync();
+
+            if (user != null)
+            {
+                var mySecret = "asdv234234^&%&^%&^hjsdfb2%%%";
+                var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                       new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                       new Claim(ClaimTypes.Name, user.FullName),
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(7),
+                    SigningCredentials = new SigningCredentials(mySecurityKey, SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(tokenHandler.WriteToken(token));
+            }
+            else
+			{
+                return StatusCode(StatusCodes.Status401Unauthorized);
+			}
         }
 
-        [HttpGet("Register")]
+        [HttpPost("Register")]
         public IActionResult Register()
         {
-            throw new NotImplementedException();
+           return Ok();
         }
     }
 }
--- Backend/Core/Helpers/BasicAuthenticationHandler.cs
+++ Backend/Core/Helpers/BasicAuthenticationHandler.cs
@@ -8,6 +8,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace Backend.Core.Helpers
 {
