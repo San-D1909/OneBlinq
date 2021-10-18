ebe8b07a107f46cb4725cbc8b7ec5d9d74df8bc5
Jorrit Deschaux <458871@student.fontys.nl>
2021-10-18 12:49:33 +0200

    navmenu only in home page now and not in layout anymore

---
+++ Front/ClientApp/src/components/Loading.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+import logo from '../wink.gif';
+
+export class Home extends Component {
+    static displayName = Home.name;
+
+    render() {
+        return (
+            <div>
+                <img src={logo} alt="loading..." />
+            </div>
+        );
+    }
+}
