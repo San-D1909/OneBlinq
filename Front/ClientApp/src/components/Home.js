65aeb62b40e991ffb4a936af70799d423944d278
Stitch <st.van.eekelen@gmail.com>
2021-10-19 14:02:20 +0200

    Changed navbar and homepage

--- Front/ClientApp/src/components/Home.js
+++ Front/ClientApp/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavMenu } from './NavMenu';
 import "./Home.css";
-import Button from 'reactstrap/lib/Button';
+import { Button } from 'react-bootstrap';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -13,8 +13,10 @@ export class Home extends Component {
             <div>
               <h1 className="homeheader">Design just got a hell <br/> of a lot <div className="headerpurple">faster</div></h1>
               <div className="btnholder">
-                <a className="btngetstarted" href="/register">Get started for free</a>
-                <a className="btnwatchvideo" href="/">Watch video</a>
+                <Button className="m-1 btn-oneblinq-roze" href="/login">Get started for free</Button>
+                <Button className="m-1" variant="outline-light" href="">Watch video</Button>
+                {/* <a className="btngetstarted" href="/register">Get started for free</a>
+                <a className="btnwatchvideo" href="/">Watch video</a> */}
               </div>
             </div>
         </>
--- Front/ClientApp/src/components/NavMenu.js
+++ Front/ClientApp/src/components/NavMenu.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import './NavMenu.css';
 import Dropdown from 'reactstrap/lib/Dropdown';
 import DropdownItem from 'reactstrap/lib/DropdownItem';
+import { Button, NavDropdown } from 'react-bootstrap';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -26,24 +27,25 @@ export class NavMenu extends Component {
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow mb-3" light>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow mb-3" dark>
             <NavbarBrand tag={Link} to="/" className="p-3"><img src="./images/logo_white_name_only.svg" /></NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2"/>
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/">Home</NavLink>
-                </NavItem>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown id="aboutdropdown" title="Plugins" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Forms</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown id="aboutdropdown" title="About" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
-                <NavItem>
-                  <NavLink tag={Link} className="btnnavgetstarted" to="/login">Get started</NavLink>
-                </NavItem>
+                <Button variant="outline-light" href="/login">Get Started</Button>
+                {/* <NavItem>
+                  <NavLink tag={Link} to="/login">Get started</NavLink>
+                </NavItem> */}
               </ul>
             </Collapse>
         </Navbar>
