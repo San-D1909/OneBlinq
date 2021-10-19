3b07c8814d683b061ffe5ad32e3217e3b29ee813
Ryan Bours <ryan.bours@gmail.com>
2021-10-19 09:35:47 +0200

    Plugin page

--- Front/ClientApp/src/App.js
+++ Front/ClientApp/src/App.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-
-import './custom.css'
+import { Plugins } from './components/Plugins';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
 
-export default class App extends Component {
-  static displayName = App.name;
+import './custom.css'
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-      </Layout>
-    );
-  }
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/plugins' component={Plugins} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
+            </Layout>
+        );
+    }
 }
