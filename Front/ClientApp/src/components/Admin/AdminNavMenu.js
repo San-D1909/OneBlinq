a4a19a73d1ed6e75424a087192d9afcf29acfe43
Tom Derks <459530@student.fontys.nl>
2021-11-01 10:50:42 +0100

    AdminDash

---
+++ Front/ClientApp/src/components/Admin/AdminNavMenu.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+
+class AdminNavMenu extends Component {
+    render() {
+        return (
+            <div>
+                <p>List Based</p>
+                <Nav vertical>
+                    <NavItem>
+                        <NavLink href="#">Link</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Link</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Another Link</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink disabled href="#">Disabled Link</NavLink>
+                    </NavItem>
+                </Nav>
+                <hr />
+                <p>Link based</p>
+                <Nav vertical>
+                    <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink> <NavLink href="#">Another Link</NavLink> <NavLink disabled href="#">Disabled Link</NavLink>
+                </Nav>
+            </div>
+        );
+    }
+}
+
+export default AdminNavMenu;
