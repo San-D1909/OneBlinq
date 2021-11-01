3f823aabf9c5cad7f8fc0f27cc88e9196fdb980d
Ryan Bours <ryan.bours@gmail.com>
2021-11-01 15:22:18 +0100

    svg  background

--- Front/ClientApp/src/components/Home.js
+++ Front/ClientApp/src/components/Home.js
@@ -2,38 +2,35 @@ import React, { Component } from 'react';
 import { NavMenu } from './NavMenu';
 import "./Home.css";
 import { Button } from 'react-bootstrap';
-//import bgimage from '~/public/images/background-image.png';
 
 export class Home extends Component {
-  static displayName = Home.name;
-
+    static displayName = Home.name;
 
     backgroundImage = {
-        background: 'url("/images/background-image.png") no-repeat',
-        backgroundSize: 'cover',
+        background: 'url("/images/background-image.svg") no-repeat',
+        backgroundSize: 'contain',
         height: '100%',
         width: '100%',
         position: 'absolute',
-        top: '0',
-        backgroundPosition: 'bottom'
+        bottom: 0,
+        backgroundPosition: 'bottom',
     }
-  render () {
-    return (
-        <>
-            <NavMenu />
-            <div style={this.backgroundImage} className="bg-homepage-image">
-                <div>
-                    <h1 className="homeheader">Design just got a hell <br/> of a lot <div className="headerpurple">faster</div></h1>
-                    <div className="btnholder">
-                    <Button className="m-1 btn-oneblinq-roze" href="/login">Get started for free</Button>
-                    <Button className="m-1" variant="outline-light" href="">Watch video</Button>
-                    {/* <a className="btngetstarted" href="/register">Get started for free</a>
-                    <a className="btnwatchvideo" href="/">Watch video</a> */}
+
+    render() {
+        return (
+            <>
+                <NavMenu />
+                <div style={this.backgroundImage} className="bg-homepage-image">
+                    <div>
+                        <h1 className="homeheader">Design just got a hell <br /> of a lot <div className="headerpurple">faster</div></h1>
+                        <div className="btnholder">
+                            <Button className="m-1 btn-oneblinq-roze" href="/login">Get started for free</Button>
+                            <Button className="m-1" variant="outline-light" href="">Watch video</Button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </>
-       );
+            </>
+        );
     }
 }
