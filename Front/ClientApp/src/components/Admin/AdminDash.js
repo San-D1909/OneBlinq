09307e7194cb11f2162a78f94eac3d802db9599c
onurinc <Onur.i@hotmail.nl>
2021-11-02 11:09:29 +0100

    Added Admin Dashboard with Mock API. Fixed Nav import.

--- Front/ClientApp/src/components/Admin/AdminDash.js
+++ Front/ClientApp/src/components/Admin/AdminDash.js
@@ -1,70 +1,18 @@
-﻿import React, { Component } from 'react';
-import AdminNavMenu from './AdminNavMenu';
-//import { AdminNavMenu } from './AdminNavMenu';
-//import { List, Datagrid, Textfield, Filter, SearchInput } from 'react-admin';
-//import { Admin, resource } from 'react-admin';
-//import lb4provider from 'react-admin-lb4';
+﻿import * as React from "react";
+import "./AdminDash.css"
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { NavMenu } from '../NavMenu';
 
-//const AdminFilter = (props) => (<Filter{...props}>
-//    <SearchInput placeholder='Username' source 'username'
-//    resettable alwaysOn />
-//       </ Filter>)
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
+const AdminDash = () => (
 
-//function AdminDash(props) {
-//    return (
-//        <List {...props/>}filters={<CustomerFilter/>}>
-//            <Datagrid>
-//                <Textfield
-//                    // Hierin comment je de rows van onze user/plugin tabel info source=''
-//                />
-//                {/* <Textfield source=''/>*/}
-//                {/* <Textfield source=''/>*/}
-//                {/* <Textfield source=''/>*/}
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users"
+            list={ListGuesser}
+        />
+    </Admin>
+);
 
-//            </Datagrid>
-
-//        </List>
-//        )
-//}
-
-
-export class AdminDash extends Component {
-    static displayName = AdminDash.name;
-
-    //fetchUserInfo() {
-    //    // TODO: fetch plugins
-    //    console.log('fetch')
-    //    return [{
-    //        id: "1",
-    //        username: "hassan"
-    //    }, {
-    //        id: "1",
-    //        username: "hassan"
-    //    }, {
-    //        id: "1",
-    //        username: "hassan"
-    //    }, {
-    //        id: "1",
-    //        username: "hassan"
-    //    }, {
-    //        id: "1",
-    //        username: "hassan"
-    //    }, {
-    //        id: "1",
-    //        username: "hassan"
-    //    }]
-    //}
-
-    render() {
-        return (
-            <>
-                <AdminNavMenu />
-            </>
-            );
-    }
-}
-            //Misschien nodig in de future (in de return())
-            //<Admin dataProvider={lb4Provider('http://localhost:4388')}<Admin />
-            //<Resource name = 'users' list = {UserList} />
-            //'https://youtu.be/4y2FFEmPW7I'
+export default AdminDash;
