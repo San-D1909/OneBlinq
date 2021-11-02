6e3a76327762a9fc25842a0bf69e84cc8fd260d3
onurinc <Onur.i@hotmail.nl>
2021-11-02 11:50:37 +0100

    Changed AdminDash to Dark Mode. added adminTheme file for future. Deleted AdminDash.css since the usage of the theme is through a js file because of React Admin framework.

--- Front/ClientApp/src/components/Admin/AdminDash.js
+++ Front/ClientApp/src/components/Admin/AdminDash.js
@@ -1,14 +1,19 @@
 ﻿import * as React from "react";
-import "./AdminDash.css"
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { NavMenu } from '../NavMenu';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark', // Switching the dark mode on is a single property value change.
+    },
+});
+
 const AdminDash = () => (
 
-    <Admin dataProvider={dataProvider}>
+    <Admin theme={theme} dataProvider={dataProvider}>
         <Resource name="users"
             list={ListGuesser}
         />
---
+++ Front/ClientApp/src/components/Admin/adminTheme.js
@@ -0,0 +1,9 @@
+﻿import * as React from "react";
+
+
+const adminTheme = () => (
+
+
+);
+
+export default adminTheme;
