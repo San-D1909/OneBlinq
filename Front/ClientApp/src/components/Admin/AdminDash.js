667dc259da27dc464670d535be44ba8db73390ca
onurinc <Onur.i@hotmail.nl>
2021-11-02 12:59:14 +0100

    Implemented ThemeOptions so the dashboard is changeable in the colours of OneBlinq in the future.

--- Front/ClientApp/src/components/Admin/AdminDash.js
+++ Front/ClientApp/src/components/Admin/AdminDash.js
@@ -2,6 +2,26 @@
 import { Admin, Resource, ListGuesser,EditGuesser} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeOptions } from '@material-ui/core';
+
+export const newOptions: ThemeOptions = {
+
+    // theme customizable at https://bareynol.github.io/mui-theme-creator
+
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#b53f41',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        background: {
+            default: '#000000',
+            paper: '#2f2f2f',
+        },
+    },
+};
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
@@ -12,8 +32,7 @@ const theme = createMuiTheme({
 });
 
 const AdminDash = () => (
-
-    <Admin theme={theme} dataProvider={dataProvider}>
+    <Admin theme={newOptions} dataProvider={dataProvider}>
         <Resource name="users"
             list={ListGuesser}
             edit={EditGuesser}
