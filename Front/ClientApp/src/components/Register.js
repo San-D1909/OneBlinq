513b2d25ec863d3d101c650174b81c0bd4c19b1f
Stitch <st.van.eekelen@gmail.com>
2021-11-08 13:28:37 +0100

    Registratie pagina aangepast Smiley op mobile view nu naar onder in plaats van boven.

--- Front/ClientApp/src/components/Register.js
+++ Front/ClientApp/src/components/Register.js
@@ -20,14 +20,7 @@ export class Register extends Component {
     return (
         <>
             <NavMenu />
-            <div className="row p-0 mx-auto registercontainer">
-                <div className="col-12 col-lg-6 p-1">
-                    <Card className="registerformcard h-100 order-last">
-                        <CardBody className="p-0">
-                            <CardImg className="h-100" src="./images/logo_big_wink_no_bg.svg" />
-                        </CardBody>
-                    </Card>
-                </div>
+            <div className="row p-0 mx-auto registercontainer d-sm-none d-flex">
                 <div className="col-12 col-lg-6 p-1">
                     <Card className="h-100">
                         <CardBody>
@@ -55,7 +48,49 @@ export class Register extends Component {
                         </CardBody>
                     </Card>
                 </div>
-
+                <div className="col-12 col-lg-6 p-1">
+                    <Card className="registerformcard h-100 order-last">
+                        <CardBody className="p-0">
+                            <CardImg className="h-100" src="./images/logo_big_wink_no_bg.svg" />
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+            <div className="row p-0 mx-auto registercontainer d-none d-sm-flex">
+                <div className="col-12 col-lg-6 p-1">
+                    <Card className="registerformcard h-100 order-last">
+                        <CardBody className="p-0">
+                            <CardImg className="h-100" src="./images/logo_big_wink_no_bg.svg" />
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col-12 col-lg-6 p-1">
+                    <Card className="h-100">
+                        <CardBody>
+                            <h1 className="text-center">Register</h1>
+                            <div className="col-12">
+                                <Form>
+                                    <div className="py-2">
+                                        <Label for="email">Email</Label>
+                                        <Input type="text" name="email" />
+                                    </div>
+                                    <div className="py-2">
+                                        <Label for="password">Password</Label>
+                                        <Input type="password" name="password" />
+                                    </div>
+                                    <div className="py-2">
+                                        <Label for="password">Confirm password</Label>
+                                        <Input type="password" name="password" />
+                                    </div>
+                                    <div className="py-2">
+                                        <Button className="my-2 mr-2 ml-0 registerbutton">Register</Button>
+                                        <Link className="m-2 loginlink" to="/login">Already have an account? Login here!</Link>
+                                    </div>
+                                </Form>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
         </>
     );
