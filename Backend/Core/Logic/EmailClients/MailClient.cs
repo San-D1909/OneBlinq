3685a821319c41c228e557f98ed23b2671fa3fb4
onurinc <onur.i@hotmail.nl>
2021-11-16 11:00:33 +0100

    Added an Interface for MailClient and added a MailController class.

--- Backend/Core/Logic/EmailClients/MailClient.cs
+++ Backend/Core/Logic/EmailClients/MailClient.cs
@@ -2,10 +2,11 @@
 using System.Threading.Tasks;
 using System.Net.Mail;
 using System.Net;
+using Backend.Core.Logic.EmailClients;
 
 namespace Backend.Core.Logic
 {
-    public class MailClient
+    public class MailClient : IMailClient
     {
         private readonly SmtpConfiguration _smtpConfiguration;
 
@@ -15,6 +16,27 @@ namespace Backend.Core.Logic
                 throw new ArgumentNullException(nameof(smtpConfiguration));
 
             _smtpConfiguration = smtpConfiguration;
+        }
+
+
+        public async Task SendEmailAsync(string from, string to, string subject, string body)
+        {
+            if (string.IsNullOrEmpty(from))
+                throw new ArgumentNullException(nameof(from));
+            if (string.IsNullOrEmpty(to))
+                throw new ArgumentNullException(nameof(to));
+            if (string.IsNullOrEmpty(subject))
+                throw new ArgumentNullException(nameof(subject));
+            if (string.IsNullOrEmpty(body))
+                throw new ArgumentNullException(nameof(body));
+
+            var message = new MailMessage();
+            message.From = new MailAddress(from);
+            message.To.Add(to);
+            message.Subject = subject;
+            message.Body = body;
+
+            await SendEmailAsync(message);
         }
 
         public async Task SendEmailAsync(MailMessage mailMessage)
@@ -36,26 +58,6 @@ namespace Backend.Core.Logic
             {
                 mailMessage.Dispose();
             }
-        }
-
-        public async Task SendEmailAsync(string from, string to, string subject, string body)
-        {
-            if (string.IsNullOrEmpty(from))
-                throw new ArgumentNullException(nameof(from));
-            if (string.IsNullOrEmpty(to))
-                throw new ArgumentNullException(nameof(to));
-            if (string.IsNullOrEmpty(subject))
-                throw new ArgumentNullException(nameof(subject));
-            if (string.IsNullOrEmpty(body))
-                throw new ArgumentNullException(nameof(body));
-
-            var message = new MailMessage();
-            message.From = new MailAddress(from);
-            message.To.Add(to);
-            message.Subject = subject;
-            message.Body = body;
-
-            await SendEmailAsync(message);
         }
     }
 }
---
+++ Backend/Models/MailModels/MailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models.MailModels
+{
+    public class MailModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
