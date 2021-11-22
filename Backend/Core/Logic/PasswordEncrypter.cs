827ff1bf4230f3d395a01d0b3a4e8818a44c6045
Jorrit Deschaux <458871@student.fontys.nl>
2021-11-22 10:03:48 +0100

    Added Password Encryption class to logic folder

---
+++ Backend/Core/Logic/PasswordEncrypter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Logic
+{
+	public class PasswordEncrypter
+	{
+
+		private IConfiguration _config;
+
+		public PasswordEncrypter(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public string EncryptPassword(string password)
+		{
+
+			string input = password + _config["Secret"];
+
+			MD5 md5Hasher = MD5.Create();
+
+			byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+			StringBuilder sBuilder = new StringBuilder(); 
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				sBuilder.Append(data[i].ToString("x2"));
+			}
+
+			return sBuilder.ToString();
+
+		}
+
+	}
+}
