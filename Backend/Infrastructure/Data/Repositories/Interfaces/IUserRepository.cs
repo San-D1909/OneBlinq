38f4db3a8b8ba9af31b65cfaf95969081841e7a3
onurinc <onur.i@hotmail.nl>
2021-11-22 14:26:49 +0100

    Added UserRepository & IUserRepository with GetUserById and UpdateUser functions for the User Info page

---
+++ Backend/Infrastructure/Data/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Models;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Data.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<UserModel> GetUserById(int UserId);
+        Task<UserModel> UpdateUser(UserModel user);
+    }
+}
---
+++ Backend/Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using Backend.Infrastructure.Data.Repositories.Interfaces;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserModel> GetUserById(int UserId)
+        {
+            try
+            {
+                var item = await _context.Set<UserModel>()
+                    .Where(x => x.UserId == UserId)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync();
+
+                if (item == null)
+                {
+                    throw new Exception($"Couldn't find entity with id={UserId}");
+                }
+
+                return item;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't retrieve entity with id={UserId}: {ex.Message}");
+            }
+        }
+
+        public async Task<UserModel> UpdateUser(UserModel user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            try
+            {
+                _context.Set<UserModel>().Update(user);
+                await _context.SaveChangesAsync();
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(user)} could not be updated: {ex.Message}");
+            }
+        }
+    }
+}
