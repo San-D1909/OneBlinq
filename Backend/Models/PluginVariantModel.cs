e7cc323a45062c892e330e26151dc8bb6a59e830
Ryan Bours <ryan.bours@gmail.com>
2021-12-14 14:50:24 +0100

    removed required from variant

--- Backend/Models/PluginVariantModel.cs
+++ Backend/Models/PluginVariantModel.cs
@@ -10,7 +10,6 @@ namespace Backend.Models
         public string Description { get; set; }
         [Required]
         public decimal Price { get; set; }
-        [Required]
         public string StripePriceId { get; set; }
     }
 }
