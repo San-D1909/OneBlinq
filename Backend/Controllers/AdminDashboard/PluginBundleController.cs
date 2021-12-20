9d302b2b40caaf193a94e81df64ed7dd574db65d
Stitch <st.van.eekelen@gmail.com>
2021-12-20 10:44:22 +0100

    Added admin endpoints for pluginbundle

---
+++ Backend/Controllers/AdminDashboard/PluginBundleController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Infrastructure.Data;
+using Backend.Models;
+
+namespace Backend.Controllers.AdminDashboard
+{
+    [Route("api/v{version:apiVersion}/admin/[controller]")]
+    [ApiVersion("1")]
+    [ApiController]
+    public class PluginBundleController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PluginBundleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PluginBundle
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PluginBundleModel>>> GetPluginBundle()
+        {
+            Request.HttpContext.Response.Headers.Add("Access-Control-Expose-Headers", "Content-Range");
+            Request.HttpContext.Response.Headers.Add("Content-Range", "pluginbundles 0-5/1");
+            Request.HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+
+            return await _context.PluginBundle.ToListAsync();
+        }
+
+        // GET: api/PluginBundle/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PluginBundleModel>> GetPluginBundleModel(int id)
+        {
+            var pluginBundleModel = await _context.PluginBundle.FindAsync(id);
+
+            if (pluginBundleModel == null)
+            {
+                return NotFound();
+            }
+
+            return pluginBundleModel;
+        }
+
+        // PUT: api/PluginBundle/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPluginBundleModel(int id, PluginBundleModel pluginBundleModel)
+        {
+            if (id != pluginBundleModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pluginBundleModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PluginBundleModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PluginBundle
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PluginBundleModel>> PostPluginBundleModel(PluginBundleModel pluginBundleModel)
+        {
+            _context.PluginBundle.Add(pluginBundleModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPluginBundleModel", new { id = pluginBundleModel.Id }, pluginBundleModel);
+        }
+
+        // DELETE: api/PluginBundle/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePluginBundleModel(int id)
+        {
+            var pluginBundleModel = await _context.PluginBundle.FindAsync(id);
+            if (pluginBundleModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.PluginBundle.Remove(pluginBundleModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PluginBundleModelExists(int id)
+        {
+            return _context.PluginBundle.Any(e => e.Id == id);
+        }
+    }
+}
