0764ff5487a253feac64e5c8d91666ba11e9c2eb
Stitch <st.van.eekelen@gmail.com>
2022-01-21 11:29:20 +0100

    Added migrations

--- Backend/Migrations/20220119120129_testing.Designer.cs
+++ Backend/Migrations/20220119120129_testing.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Backend.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220119120129_testing")]
+    partial class testing
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
