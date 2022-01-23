3000fb45c2180d688a460e5b508a4fe10d90f937
San-D1909 <sanderaarts19992@gmail.com>
2022-01-23 15:29:34 +0100

    Seeders for both basic plugins

--- Backend/Core/DatabaseSeeders/PluginVariantSeeder.cs
+++ Backend/Core/DatabaseSeeders/PluginVariantSeeder.cs
@@ -11,25 +11,38 @@ namespace Backend.Core.DatabaseSeeders
 {
     public class PluginVariantSeeder
     {
-        public static PluginVariantModel PLUGIN_VARIANT_FORMS;
+        public static PluginVariantModel PLUGIN_VARIANT_FORMS1;
+        public static PluginVariantModel PLUGIN_VARIANT_FORMS2;
 
         public static void SeedData(ApplicationDbContext context)
         {
             if (context.PluginVariant.ToList().Count == 0)
             {
-                PLUGIN_VARIANT_FORMS = new PluginVariantModel
+                PLUGIN_VARIANT_FORMS1 = new PluginVariantModel
                 {
                     Id = 1,
-                    StripePriceId = "adf",
+                    StripePriceId = PluginSeeder.PLUGIN_FORMS.StripeProductId,
                     Description = "Variant",
                     IsSubscription = true,
                     MaxActivations = 10,
                     Plugin = PluginSeeder.PLUGIN_FORMS,
-                    PluginId = 1,
+                    PluginId = PluginSeeder.PLUGIN_FORMS.Id,
                     Price = 69
+                };                
+                PLUGIN_VARIANT_FORMS2 = new PluginVariantModel
+                {
+                    Id = 2,
+                    StripePriceId = PluginSeeder.PLUGIN_LINEHEIGHT.StripeProductId,
+                    Description = "Variant",
+                    IsSubscription = true,
+                    MaxActivations = 100,
+                    Plugin = PluginSeeder.PLUGIN_LINEHEIGHT,
+                    PluginId = PluginSeeder.PLUGIN_LINEHEIGHT.Id,
+                    Price = 21
                 };
 
-                context.Add(PLUGIN_VARIANT_FORMS);
+                context.Add(PLUGIN_VARIANT_FORMS1);
+                context.Add(PLUGIN_VARIANT_FORMS2);
                 context.SaveChanges();
             }
         }
