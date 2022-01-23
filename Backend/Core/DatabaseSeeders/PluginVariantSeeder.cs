632853ebec7a40534a69e13204052084178b020c
San-D1909 <sanderaarts19992@gmail.com>
2022-01-23 15:24:42 +0100

    Seed a variant

---
+++ Backend/Core/DatabaseSeeders/PluginVariantSeeder.cs
@@ -0,0 +1,37 @@
+﻿using Backend.Core.Logic;
+using Backend.Infrastructure.Data;
+using Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Backend.Core.DatabaseSeeders
+{
+    public class PluginVariantSeeder
+    {
+        public static PluginVariantModel PLUGIN_VARIANT_FORMS;
+
+        public static void SeedData(ApplicationDbContext context)
+        {
+            if (context.PluginVariant.ToList().Count == 0)
+            {
+                PLUGIN_VARIANT_FORMS = new PluginVariantModel
+                {
+                    Id = 1,
+                    StripePriceId = "adf",
+                    Description = "Variant",
+                    IsSubscription = true,
+                    MaxActivations = 10,
+                    Plugin = PluginSeeder.PLUGIN_FORMS,
+                    PluginId = 1,
+                    Price = 69
+                };
+
+                context.Add(PLUGIN_VARIANT_FORMS);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -100,6 +100,7 @@ namespace Backend
                 context.Database.Migrate();
                 UserSeeder.SeedData(context, new PasswordEncrypter(this.Configuration));
                 PluginSeeder.SeedData(context);
+                PluginVariantSeeder.SeedData(context);
                 LicenseSeeder.SeedData(context);
                 PluginLicenseSeeder.SeedData(context);
                 PluginBundleSeeder.SeedData(context);
